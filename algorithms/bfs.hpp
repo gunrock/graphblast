b8c816d46fff7093969c6858df906f30d2aceaaa
Carl Yang <ctcyang@ucdavis.edu>
2017-11-17 01:56:38 -0800

    Add GrB_ALL. Working on bfs

--- algorithms/bfs.hpp
+++ algorithms/bfs.hpp
@@ -3,14 +3,33 @@
 
 namespace graphblas
 {
-  Info bfs( const Matrix* A, Index s )
+  Info bfs( const graphblas::Matrix<bool>* A, graphblas::Index s )
   {
-    Index n;
-    CHECK( A.nrows( &n ) );
+    graphblas::Index n;
+    CHECK( A->nrows( &n ) );
 
-    Vector v(n);
+    graphblas::Vector<int>  v(n);
+    graphblas::Vector<bool> q(n);
+    std::vector<Index> indices(1,s);
+    std::vector<bool>  values(1,true);
+    CHECK( q.build( &indices, &values, 1 ) );
+
+    graphblas::BinaryOp GrB_LOR(  graphblas::logical_or() );
+    graphblas::BinaryOp GrB_LAND( graphblas::logical_and() );
+    graphblas::Monoid   GrB_Lor( GrB_LOR, false );
+    graphblas::Semiring GrB_Boolean( GrB_Lor, GrB_LAND );
+
+    graphblas::Descriptor desc_nomask;
+
+    graphblas::Index d = 0;
+    bool succ = false;
+    do
+    {
+      d++;
+      CHECK( assign( &v, &q, GrB_NULL, d, GrB_ALL, n, GrB_NULL );
+
+    } while( succ );
   }
-
 }  // graphblas
 
 #endif  // GRB_BFS_HPP
