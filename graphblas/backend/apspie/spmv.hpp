9a3700b08ce380c40d0c31505bf9361cd8ba54db
Carl Yang <ctcyang@ucdavis.edu>
2017-12-02 01:46:55 -0800

    Added temporary testing code to spmv for testing Stephen Jones's suggestions. Next, write specialization for GrB_OR + GrB_AND

--- graphblas/backend/apspie/spmv.hpp
+++ graphblas/backend/apspie/spmv.hpp
@@ -39,6 +39,26 @@ namespace backend
     *val = mul_op(*val,*val);
   }
 
+  template <typename MulOp, typename AddOp, typename T>
+  void cpu_kernel( MulOp mul_op, AddOp add_op, T* val )
+  {
+    *val = add_op(*val,*val);
+    *val = mul_op(*val,*val);
+  }
+
+typedef float (*op_func_t) (float, float);
+__device__ float add_func(float x, float y) { return x + y; }
+__device__ float mul_func(float x, float y) { return x * y; }
+__device__ op_func_t p_add_func = add_func;
+__device__ op_func_t p_mul_func = mul_func;
+
+  template <typename MulOp, typename AddOp, typename T>
+  __global__ void gpu_kernel( MulOp mul_op, AddOp add_op, T* val )
+  {
+    *val = (*add_op)(*val,*val);
+    *val = (*mul_op)(*val,*val);
+  }
+
   template <typename W, typename a, typename U, typename M,
             typename BinaryOpT,      typename SemiringT>
   Info spmv( DenseVector<W>*        w,
@@ -54,16 +74,35 @@ namespace backend
     //bool useScmp = (
     //bool useRepl = (
     //bool useTran = (desc->
-    if( !useMask )
+    if( useMask )
+    {
+      
+    }
+    else if( !useMask )
     {
       a* d_val;
       a  h_val = 1.f;
       CUDA( cudaMalloc( &d_val, sizeof(a) ) );
       CUDA( cudaMemcpy( d_val, &h_val, sizeof(a), cudaMemcpyHostToDevice ) );
-      kernel<<<1,1>>>( op->mul_, op->add_, d_val );
+
+      op_func_t h_add_func;
+      op_func_t h_mul_func;
+      cudaMemcpyFromSymbol( &h_add_func, p_add_func, sizeof( op_func_t ) );
+      cudaMemcpyFromSymbol( &h_mul_func, p_mul_func, sizeof( op_func_t ) );
+      op_func_t d_add_func = h_add_func;
+      op_func_t d_mul_func = h_mul_func;
+
+      //cpu_kernel( graphblas::multiplies<a>(), graphblas::plus<a>(), &h_val);
+      //cpu_kernel( op->mul_, op->add_, &h_val );
+      //kernel<<<1,1>>>( op->mul_, op->add_, d_val );
+      gpu_kernel<<<1,1>>>( d_mul_func, d_add_func, d_val );
       //kernel<<<1,1>>>( graphblas::multiplies<a>(), graphblas::plus<a>(), d_val );
       CUDA( cudaMemcpy( &h_val, d_val, sizeof(a), cudaMemcpyDeviceToHost ) );
       std::cout << h_val << std::endl;
+      std::cout << &op << " " << &u << " " << &w << std::endl;
+      std::cout << &(op->mul_) << " " << &(op->add_) << std::endl;
+      std::cout << &(u->d_val_) << " " << &(w->d_val_) << std::endl;
+      std::cout << &h_add_func << " " << &p_add_func << std::endl;
       /*mgpu::SpmvCsrBinary( A->d_csrVal_, A->d_csrColInd_, A->nvals_, 
           A->d_csrRowPtr_, A->nrows_, u->d_val_, true, w->d_val_, 
           op->identity(), mgpu::multiplies<a>(), mgpu::plus<a>(), 
