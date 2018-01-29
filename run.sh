5d45d9b6c80afcb6ad08c538d0ae17c01f37178c
Carl Yang <ctcyang@ucdavis.edu>
2018-01-29 00:22:15 -0800

    change order of datasets

--- run.sh
+++ run.sh
@@ -14,20 +14,20 @@ do
     fi
 done
 
-for j in 0 1 2
+for j in 1 #0 1 2
 do
   #for i in soc-orkut soc-LiveJournal1 hollywood-2009 indochina-2004 kron_g500-logn21 roadNet-CA
-  for i in soc-orkut soc-LiveJournal1 hollywood-2009 indochina-2004 kron_g500-logn21 rmat_n22_e64 rmat_n23_e32 rmat_n24_e16 rgg_n24_0.000548 road_usa roadNet-CA
+  for i in soc-orkut soc-LiveJournal1 hollywood-2009 indochina-2004 kron_g500-logn21 rmat_n22_e64 rmat_n23_e32 rmat_n24_e16 rgg_n24_0.000548 roadNet-CA road_usa
   do
     if [ "$ARCH" = "GEN_SM45" ] ; then
-      bin/gbfs --struconly=true --mxvmode=$j --timing=1 /data-2/topc-datasets/$i.mtx
+      bin/gbfs --sort=0 --struconly=true --mxvmode=$j --timing=1 /data-2/topc-datasets/$i.mtx
     fi
   done
 done
 
 # IPDPS 2016
 #for i in soc-orkut hollywood-2009 indochina-2004 kron_g500-logn21 rgg_n24_0.000548 roadNet-CA
-for i in soc-orkut soc-LiveJournal1 hollywood-2009 indochina-2004 kron_g500-logn21 rmat_n22_e64 rmat_n23_e32 rmat_n24_e16 rgg_n24_0.000548 road_usa roadNet-CA
+for i in soc-orkut soc-LiveJournal1 hollywood-2009 indochina-2004 kron_g500-logn21 rmat_n22_e64 rmat_n23_e32 rmat_n24_e16 rgg_n24_0.000548 roadNet-CA road_usa
 do
   for j in 0 1 2
   do
