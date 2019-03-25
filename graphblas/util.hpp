2412c351ef5776931e5bded8556528331a72cabb
Carl Yang <ctcyang@ucdavis.edu>
2019-03-24 21:14:34 -0700

    fix directed graph selector. Working on fixing directed push-only bfs

--- graphblas/util.hpp
+++ graphblas/util.hpp
@@ -104,8 +104,7 @@ void parseArgs(int argc, char**argv, po::variables_map* vm) {
     ("maxcolors", po::value<int>()->default_value(10000),
         "Upper bound on colors when graph coloring algorithm is used")
     ("gcalgo", po::value<int>()->default_value(0),
-        "0: Jones-Plassman, 1: Maximal independent set, 2: Independent set,
-         3: Fused apply-vxm")
+        "0: Jones-Plassman, 1: Maximal independent set, 2: Independent set")
 
     // GPU params
     ("nthread", po::value<int>()->default_value(128),
@@ -387,6 +386,7 @@ int readMtx(const char*                    fname,
   printf("Undirected due to mtx: %d\n", mm_is_symmetric(matcode));
   printf("Undirected due to cmd: %d\n", directed == 2);
   bool is_undirected = mm_is_symmetric(matcode) || directed == 2;
+  is_undirected = (directed == 1) ? false : is_undirected;
   printf("Undirected: %d\n", is_undirected);
   if (dat_name != NULL)
     *dat_name = convert(fname, is_undirected);
