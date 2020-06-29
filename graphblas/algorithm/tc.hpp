b02b77aa8e0c203c920b2d8ef16aabb053fa7e9e
Carl Yang <carl14706@gmail.com>
2020-06-28 22:35:35 -0700

    Revert "working on TC"
    This reverts commit 7827d2082bf5f63e448a77004347bf8abd2eb202.

--- graphblas/algorithm/tc.hpp
+++ graphblas/algorithm/tc.hpp
@@ -12,38 +12,74 @@ namespace graphblas {
 namespace algorithm {
 
 // Use int for ntris and A
-float tc(int*               ntris,
-         const Matrix<int>* A,     // lower triangular matrix
-         Descriptor*        desc) {
+float tc(int*                 ntris,
+         const Matrix<float>* A,     // upper triangular matrix
+         Descriptor*          desc) {
   // Get number of vertices
   Index A_nrows;
   CHECK(A->nrows(&A_nrows));
 
-  Matrix<int> B(A_nrows, A_nrows);
+  // Pagerank vector (p)
+  CHECK(p->clear());
+  CHECK(p->fill(1.f/A_nrows));
 
-  // Set second input to be transposed
-  CHECK(desc->toggle(graphblas::GrB_INP1));
+  // Previous pagerank vector (p_tcev)
+  Vector<float> p_tcev(A_nrows);
 
-  int iter = 1;
+  // Temporary pagerank (p_temp)
+  Vector<float> p_swap(A_nrows);
+
+  // Residual vector (r)
+  Vector<float> r(A_nrows);
+  r.fill(1.f);
+
+  // Temporary residual (r_temp)
+  Vector<float> r_temp(A_nrows);
+
+  int iter;
   float error_last = 0.f;
   float error = 1.f;
+  Index unvisited = A_nrows;
 
   backend::GpuTimer gpu_tight;
   float gpu_tight_time = 0.f;
   gpu_tight.Start();
 
-  if (desc->descriptor_.debug())
-    std::cout << "=====TC Iteration " << iter - 1 << "=====\n";
+  for (iter = 1; error > eps && iter <= desc->descriptor_.max_niter_; ++iter) {
+    if (desc->descriptor_.debug())
+      std::cout << "=====TC Iteration " << iter - 1 << "=====\n";
+    gpu_tight.Stop();
+    if (iter > 1) {
+      std::string vxm_mode = (desc->descriptor_.lastmxv_ == GrB_PUSHONLY) ?
+          "push" : "pull";
+      if (desc->descriptor_.timing_ == 1)
+        std::cout << iter - 1 << ", " << error << "/" << A_nrows << ", "
+            << unvisited << ", " << vxm_mode << ", "
+            << gpu_tight.ElapsedMillis() << "\n";
+      gpu_tight_time += gpu_tight.ElapsedMillis();
+    }
+    unvisited -= static_cast<int>(error);
+    gpu_tight.Start();
+    error_last = error;
+    p_tcev = *p;
 
-  // B = A * A^T
-  mxm<int, int, int, int>(&B, A, GrB_NULL, PlusMultipliesSemiring<int>(),
-      A, A, desc);
+    // p = A*p + (1-alpha)*1
+    vxm<float, float, float, float>(&p_swap, GrB_NULL, GrB_NULL,
+        PlusMultipliesSemiring<float>(), &p_tcev, A, desc);
+    eWiseAdd<float, float, float, float>(p, GrB_NULL, GrB_NULL,
+        PlusMultipliesSemiring<float>(), &p_swap, (1.f-alpha)/A_nrows, desc);
 
-  // ntris = reduce(B)
-  reduce<int, int>(ntris, GrB_NULL, PlusMonoid<int>(), &B, desc);
+    // error = l2loss(p, p_tcev)
+    eWiseMult<float, float, float, float>(&r, GrB_NULL, GrB_NULL,
+        PlusMinusSemiring<float>(), p, &p_tcev, desc);
+    eWiseAdd<float, float, float, float>(&r_temp, GrB_NULL, GrB_NULL,
+        MultipliesMultipliesSemiring<float>(), &r, &r, desc);
+    reduce<float, float>(&error, GrB_NULL, PlusMonoid<float>(), &r_temp, desc);
+    error = sqrt(error);
 
-  if (desc->descriptor_.debug())
-    std::cout << "ntris: " << *ntris << std::endl;
+    if (desc->descriptor_.debug())
+      std::cout << "error: " << error_last << std::endl;
+  }
   gpu_tight.Stop();
   std::string vxm_mode = (desc->descriptor_.lastmxv_ == GrB_PUSHONLY) ?
       "push" : "pull";
